432fec87ea832341e6c04a8f7d27b62ff33fa6f5
Monica <monica.b.raju001@gmail.com>
2024-11-18 09:42:19 +0530

    updated css

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -101,7 +101,7 @@ class Hero extends Component {
                 <div className="container">
                   <div className="hero-text">
                     
-                    <h2 style={{color:'black',fontSize:'50px'}}>
+                    <h2 style={{color:'black',fontFamily:'Areal,sans-serif',fontSize:'35px'}}>
                     Invest in Farmland for <br/>
                     Long-Term Prosperity
                     </h2>
